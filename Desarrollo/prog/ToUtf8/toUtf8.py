f71fd35f4e2ab3918288436d32842d10a56c29d8
Nahuel <persianahuel@gmail.com>
2016-06-08 09:20:46 -0300

    terminado el toUtf8.py

--- Desarrollo/prog/ToUtf8/toUtf8.py
+++ Desarrollo/prog/ToUtf8/toUtf8.py
@@ -1,8 +1,8 @@
 import os
 
 FILE="./pruebas/Fringe.srt"
-print os.popen("file "+FILE)
-if "8859" in os.popen("file "+FILE):
-	os.system("iconv -f UTF-8859-1 -t UTF-8 "+FILE+" -o "+FILE+".utf8")
+if "8859" in os.popen("file "+FILE).read():
+	os.system("iconv -f ISO-8859-1 -t UTF-8 "+FILE+" -o "+FILE+".utf8")
 	os.system("rm "+FILE)
 	os.system("mv "+FILE+".utf8 "+FILE)
+	print 'Done'
