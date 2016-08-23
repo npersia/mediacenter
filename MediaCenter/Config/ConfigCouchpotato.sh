87b1935b1252355bcd50469084b449e2245d1060
Your Name <you@example.com>
2016-08-23 09:09:09 -0300

    modificaciones

--- MediaCenter/Config/ConfigCouchpotato.sh
+++ MediaCenter/Config/ConfigCouchpotato.sh
@@ -2,6 +2,14 @@ FOLDER=$1
 
 docker pull linuxserver/couchpotato
 
-docker create --name=couchpotato -v $FOLDER/couchpotato/config:/config -v $FOLDER/downloads:/downloads -v $FOLDER/couchpotato/movies:/movies -e PGID=0 -e PUID=0 -e TZ=London -p 5050:5050 linuxserver/couchpotato
+docker create \
+	--name=couchpotato \
+	-v $FOLDER/couchpotato/config:/config \
+	-v $FOLDER/downloads:/downloads \
+	-v $FOLDER/couchpotato/movies:/movies \
+	-e PGID=0 -e PUID=0 \
+	-e TZ=London \
+	-p 5050:5050 \
+	linuxserver/couchpotato
 
 docker start couchpotato
--- MediaCenter/Config/ConfigTransmission.sh
+++ MediaCenter/Config/ConfigTransmission.sh
@@ -4,14 +4,14 @@ docker pull linuxserver/transmission
 
 
 docker create --name=transmission \
--v $FOLDER/transmission/config:/config \
--v $FOLDER/downloads:/downloads \
--v $FOLDER/transmission/watch:/watch \
--e PGID=0 -e PUID=0 \
--e TZ=London \
--p 9091:9091 -p 51413:51413 \
--p 51413:51413/udp \
-linuxserver/transmission
+	-v $FOLDER/transmission/config:/config \
+	-v $FOLDER/downloads:/downloads \
+	-v $FOLDER/transmission/watch:/watch \
+	-e PGID=0 -e PUID=0 \
+	-e TZ=London \
+	-p 9091:9091 -p 51413:51413 \
+	-p 51413:51413/udp \
+	linuxserver/transmission
 
 
 docker start transmission
